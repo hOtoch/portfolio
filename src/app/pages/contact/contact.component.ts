17fc65d33cb5147bf662cb50df8394aaeb255117
hOtoch <hugootoch@hotmail.com>
2024-10-22 17:48:57 -0300

    TEST: fixing email bug

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -14,7 +14,7 @@ export class ContactComponent {
     // Configuração para o EmailJS
     const serviceID = (window as any).NG_APP_SERVICE_ID;
     const templateID = (window as any).NG_APP_TEMPLATE_ID;
-    const userID = (window as any).NG_APP_USER_ID;
+    const publicKey = (window as any).NG_APP_PUBLIC_KEY;
     
     let params = {
       name : (document.getElementById('name') as HTMLInputElement).value,
@@ -25,10 +25,13 @@ export class ContactComponent {
 
 
     // Enviar email usando o EmailJS
-    emailjs.send(serviceID, templateID, params, userID)
+    emailjs.send(serviceID, templateID, params, publicKey)
       .then((result) => {
         alert('Email enviado com sucesso!');
       }, (error) => {
+        console.log('serviceID', serviceID);
+        console.log('templateID', templateID);
+        console.log('publicKey', publicKey);
         console.log('Erro ao enviar email:', error);
       });
   }
