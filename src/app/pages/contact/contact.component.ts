2757c0cc42155569c85b8193566488b09dbd09f7
hOtoch <hugootoch@hotmail.com>
2024-10-22 18:08:51 -0300

    TEST: fixing email bug

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import emailjs from '@emailjs/browser';
-
+import { environment } from 'src/environments/environment.vercel';
 
 @Component({
   selector: 'app-contact',
@@ -12,9 +12,9 @@ export class ContactComponent {
   enviarEmail(event: Event) {
     event.preventDefault();
     // Configuração para o EmailJS
-    const serviceID = (window as any).NG_APP_SERVICE_ID;
-    const templateID = (window as any).NG_APP_TEMPLATE_ID;
-    const publicKey = (window as any).NG_APP_PUBLIC_KEY;
+    const serviceID = environment.serviceID;
+    const templateID = environment.templateID;
+    const publicKey = environment.publicKey;
     
     let params = {
       name : (document.getElementById('name') as HTMLInputElement).value,
